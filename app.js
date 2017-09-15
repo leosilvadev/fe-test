0e330909893aab35621b70a7c24077879f3e1ecc
Leonardo Gomes da Silva <leonardo.gomesdasilva@finleap.com>
2017-09-15 11:41:43 +0200

    changes

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 const request = require('request');
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
+const uuidv4 = require('uuid/v4');
 
 const API_KEY = process.env.API_KEY;
 const API_BASE_URL = 'https://api.themoviedb.org/3';
@@ -11,28 +12,35 @@ const IMG_BASE_URL = 'https://image.tmdb.org/t/p/w500';
 
 app.use(cors());
 
-app.get('/api/v1/movies/:name', (req, res) => {
-  const name = req.params.name;
-  request(`${API_BASE_URL}/search/movie?api_key=${API_KEY}&query=${name}`, (error, response, body) => {
+app.get('/api/v1/movies', (req, res) => {
+  const query = req.query.name;
+  if (query === null || query === '') {
+    res.json({});
+    return;
+  }
+
+  request(`${API_BASE_URL}/search/movie?api_key=${API_KEY}&query=${query}`, (error, response, body) => {
+    const token = require('uuid/v4');
     const movies = JSON.parse(body).results.map(movie => {
       movie.full_poster_path = `${IMG_BASE_URL}/${movie.poster_path}`;
       movie.full_backdrop_path = `${IMG_BASE_URL}/${movie.backdrop_path}`;
       return movie;
     });
 
-    const [head, ...tail] = movies;
+    const [first, ...tail] = movies;
     publishNewMovie(name, tail);
 
-    res.json(head);
+    const result = Object.assign({}, first, {listening_token: token});
+    res.json(result);
   });
 });
 
-const publishNewMovie = (name, movies) => {
+const publishNewMovie = (token, movies) => {
   if (movies.length > 0) {
     setTimeout(() => {
-      const [head, ...tail] = movies;
-      const event = Object.assign({}, head, {status: 'active'});
-      io.sockets.emit(`movies.${name}`, JSON.stringify(event));
+      const [first, ...tail] = movies;
+      const event = Object.assign({}, first, {status: 'active'});
+      io.sockets.emit(`movies.${token}`, JSON.stringify(event));
       publishNewMovie(name, tail);
     }, 5000);
   } else {
